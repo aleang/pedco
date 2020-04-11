2d72f5de498251d591b84788e6348f5389e9e2f4
Pheng Taing <phengtaing92@gmail.com>
2020-04-11 23:06:59 +0100

    First iteration of Photos component
    Add photos
    caption + controller bar
    map

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { CountriesComponent } from './countries/countries.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BlogComponent } from './blog/blog.component';
 import { StatisticsComponent } from './statistics/statistics.component';
+import { PhotoComponent } from './photo/photo.component';
 
 const routes: Routes = [
   { path: 'map', component: CountriesComponent },
   { path: 'blog', component: BlogComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'photo', component:PhotoComponent },
   { path: 'statistics', component: StatisticsComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
---
+++ src/assets/photo-data.ts
@@ -0,0 +1,8 @@
+export const PHOTOS = [
+    'cn-tianjin.jpg',
+    'cn-camp.jpg',
+    'cn-xian.jpg',
+    'cn-valley.jpg',    
+    'cn-gang.jpg',
+    'cn-teddy.jpg',
+];
